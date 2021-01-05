460bd7acf1080f641750ec9cbab7154ab4985f75
YSRKEN <ysr.ken@gmail.com>
2021-01-05 16:54:55 +0900

    [add] LRAの計算コードを移植した

--- src/App.tsx
+++ src/App.tsx
@@ -27,6 +27,95 @@ const decodeAudioFromArrayBuffer = (arrayBuffer: ArrayBuffer): Promise<AudioBuff
   });
 };
 
+const calcSignalLfilter = (b: number[], a: number[], data: Float32Array): Float32Array => {
+  const outputData = new Array<number>(data.length);
+  for (let i = 0; i < data.length; i += 1) {
+    let sum = 0.0;
+    for (let m = 0; m < b.length; m += 1) {
+      const j = i - m;
+      if (j >= 0) {
+        sum += b[m] * data[i - m];
+      }
+    }
+    for (let n = 1; n < a.length; n += 1) {
+      const j = i - n;
+      if (j >= 0) {
+        sum -= a[n] * data[i - n];
+      }
+    }
+    outputData[i] = sum / a[0];
+  }
+  return new Float32Array(outputData);
+};
+
+const arrayDot = (a: number[] | Float32Array, b: number[] | Float32Array) => {
+  let sum = 0.0;
+  const len = a.length;
+  for (let i = 0; i < len; i += 1) {
+    sum += a[i] * b[i];
+  }
+  return sum;
+};
+
+const arraySum = (a: number[] | Float32Array) => {
+  let sum = 0.0;
+  const len = a.length;
+  for (let i = 0; i < len; i += 1) {
+    sum += a[i];
+  }
+  return sum;
+};
+
+/**
+ * LRAを計算する。移植前コードを書いた人：avaris(Twitter ID: @Plz_Unlock_Me)
+ */
+const calcLRA = (pcmDataL: Float32Array, pcmDataR: Float32Array, samplingRate: number) => {
+  // K - weighting filter
+  const A = Math.pow(10, 4.0 / 40);
+  let w0 = 2.0 * Math.PI * (1500.0 / samplingRate);
+  let alpha = Math.sin(w0) / (2.0 / Math.sqrt(2));
+  const b1 = [A * ((A + 1) + (A - 1) * Math.cos(w0) + 2 * Math.sqrt(A) * alpha), -2 * A * ((A - 1) + (A + 1) * Math.cos(w0)), A * ((A + 1) + (A - 1) * Math.cos(w0) - 2 * Math.sqrt(A) * alpha)];
+  const a1 = [(A + 1) - (A - 1) * Math.cos(w0) + 2 * Math.sqrt(A) * alpha, 2 * ((A - 1) - (A + 1) * Math.cos(w0)), (A + 1) - (A - 1) * Math.cos(w0) - 2 * Math.sqrt(A) * alpha];
+  w0 = 2.0 * Math.PI * (38 / samplingRate);
+  alpha = Math.sin(w0) / (2.0 * 0.5);
+  const b2 = [(1 + Math.cos(w0)) / 2, -(1 + Math.cos(w0)), (1 + Math.cos(w0)) / 2];
+  const a2 = [1 + alpha, -2 * Math.cos(w0), 1 - alpha];
+
+  const pcmDataL2 = calcSignalLfilter(b1, a1, pcmDataL);
+  const pcmDataR2 = calcSignalLfilter(b1, a1, pcmDataR);
+  const pcmDataL3 = calcSignalLfilter(b2, a2, pcmDataL2);
+  const pcmDataR3 = calcSignalLfilter(b2, a2, pcmDataR2);
+
+  // short term loudness
+  // calcurate square average in 3000ms
+  // shift: 100ms (overlap: 2900ms)
+  // "the exact amount of overlap is implementation-dependent." in EBU Tech 3342
+  const shift = Math.floor(1.0 * samplingRate / 10);
+  const calcRange = 30;
+  const N = Math.floor(10.0 * pcmDataL.length / samplingRate) - calcRange + 1;
+  let stl = new Array<number>(N); // stl: short term loudness
+  // Is zero padding needed?
+  for (let i = 0; i < N; i += 1) {
+    const tmpL = pcmDataL3.slice(i * shift, (i + calcRange) * shift);
+    const tmpR = pcmDataR3.slice(i * shift, (i + calcRange) * shift);
+    stl[i] = (arrayDot(tmpL, tmpL) + arrayDot(tmpR, tmpR)) / (3 * samplingRate);
+  }
+  // power to loudness
+  // 0.691 is a magic number for regularization
+  stl = stl.map(s => Math.log10(s) * 10 - 0.691);
+
+  // gating
+  const absoluteThreshold = -70;
+  stl = stl.filter(s => s > absoluteThreshold);
+  const I = Math.log10(arraySum(stl.map(s => Math.pow(10.0, s / 10)))) * 10 / stl.length;
+  const relativeThreshold = I - 20;
+  stl = stl.filter(s => s > relativeThreshold);
+
+  // output
+  stl.sort();
+  return stl[Math.floor(stl.length * 0.95)] - stl[Math.floor(stl.length * 0.10)];
+};
+
 const App: React.FC = () => {
   const [fileData, setFileData] = useState<File | null>(null);
   const fileInput = useRef<HTMLInputElement>(null);
@@ -54,7 +143,8 @@ const App: React.FC = () => {
     const arrayBuffer = await readFileAsArrayBuffer(fileData);
     // オーディオデータとしてパースする
     const audioData = await decodeAudioFromArrayBuffer(arrayBuffer);
-    console.log(audioData);
+    // LRAを計算する
+    console.log(`LRA：${calcLRA(audioData.getChannelData(0), audioData.getChannelData(1), audioData.sampleRate)}[LU]`);
   };
 
   return <Container>
