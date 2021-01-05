7bb8b1ec20f13f59b8ffdcaad6e7fd60714d74e9
YSRKEN <ysr.ken@gmail.com>
2021-01-05 15:03:19 +0900

    [add] ざっくりUIを作成した

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,22 @@
 import React from 'react';
-import { Col, Container, Row } from 'react-bootstrap';
+import { Button, Col, Container, Form, Row } from 'react-bootstrap';
 
 const App: React.FC = () => <Container>
   <Row className="my-3">
     <Col className="text-center">
       <h1>LRAを計算</h1>
+    </Col>
+  </Row>
+  <Row className="my-3">
+    <Col sm={6} className="mx-auto">
+      <Form>
+        <Form.Group>
+          <Form.File label="音楽ファイルを選択してください。" />
+        </Form.Group>
+        <Form.Group>
+          <Button>解析開始</Button>
+        </Form.Group>
+      </Form>
     </Col>
   </Row>
 </Container>
